42bc9acda0d73940e188c0e305def67ff2722838
Dushyant Goyal <goyaldushyant1611.com>
2025-08-20 18:25:06 +0530

    Created a folder only for JavaScript files

---
+++ JavaScript/conversion.js
@@ -0,0 +1,8 @@
+// Converting String into Number
+let score = "33"
+
+console.log(`Before Conversion: ${typeof score}`)
+
+let valueInNumber = Number(score)
+console.log(`After Conversion: ${typeof valueInNumber}`)
+
